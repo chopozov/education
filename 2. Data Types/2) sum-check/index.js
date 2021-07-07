a886fe26edb4b0936862f960aeca9c832010827e
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-07 17:48:59 +0300

    add program sum check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,14 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+  try {
+      if (typeof a === 'number' && typeof b === 'number') {
+          return a + b;
+      }
+      throw new Error("Wrong arguments type!");
+
+  }
+  catch (error) {
+      throw new Error(error)
+  }
 }
 
 window.sumCheck = sumCheck;
