3dcc74e84b58cfc9e7b6a2dc868a099236fa6821
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-07 16:58:57 +0300

    search-string v2

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,11 +1,11 @@
 function searchString(obj) {
     let str = '?';
-    Object.keys(obj).forEach(function(key){
-        if (obj[key] !== null && obj[key] !== undefined && obj[key] !== ''){
+    Object.keys(obj).forEach(function(key) {
+        if (obj[key] !== null && obj[key] !== undefined && obj[key] !== '') {
             str = str + String(key) + '=' + obj[key] + '&';
-            }
+        }
     });
-    return(str.substring(0, str.length - 1));
+    return str.substring(0, str.length - 1);
 };
 
 window.searchString = searchString;
