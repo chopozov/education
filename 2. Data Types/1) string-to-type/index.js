c89ba780f8a74adf208940b72e07ee79cbee820f
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-07 16:09:03 +0300

    add program string-to-type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,5 +1,16 @@
 function stringToType(str) {
-  // Напиши свой код здесь
+    if (str === 'true'){
+        alert('Boolean');
+    } else if(str === 'false'){
+        alert('true');
+    } else if(str === 'null'){
+        alert(null);
+    } else if(str === 'undefined'){
+        alert(undefined);
+    } else {
+        alert(str)
+    }
+    return str;
 };
 
 window.stringToType = stringToType;
