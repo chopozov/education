6c88cb6648f1bb2e31953b8afe2d24ac13849244
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-07 11:40:26 +0300

    add program is-odd

--- 1. Basic/2) is-odd/index.js
+++ 1. Basic/2) is-odd/index.js
@@ -1,7 +1,9 @@
 function isOdd(a) {
-  //Напиши свой код здесь
+    if (a % 2 === 0) {
+            return true;
+    } else {
+      return false;
+    }
 }
-
 window.isOdd = isOdd;
-
 export default isOdd;
