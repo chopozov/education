a706b9d5139cae0e76fb9ac2403fdcc8f0140e2f
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-07 17:47:56 +0300

    string-to-type v2

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,18 +1,17 @@
 function stringToType(str) {
-    if (str === 'true'){
-        return  true;
-    } else if(str === 'false'){
-        return false;
-    } else if(str === 'null'){
-        return null;
-    } else if(str === 'undefined'){
-        return undefined;
-    } else if (isNaN(str) === false && str !== ''){
-        return Number(str);
-    } else if (str === ""){
-      return '';
-    } else {
-        return String(str);
+    switch (str) {
+        case 'false':
+            return false;
+        case 'true':
+            return true;
+        case 'null':
+            return null;
+        case 'undefined':
+            return undefined;
+        case '': return ''
+        default: {
+            return !!isNaN(str) ? str : Number(str);
+        }
     }
 };
 
