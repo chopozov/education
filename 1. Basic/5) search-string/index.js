84bd30ccb87c51dd5f2465673166b555b5925260
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-07 15:31:43 +0300

    add program search-string with

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,7 +1,14 @@
 function searchString(obj) {
-  // Напиши свой код здесь
-}
+    let str = '?';
+    Object.keys(obj).forEach(function(key){
+        if (obj[key] !== null && obj[key] !== undefined && obj[key] !== ''){
+            str = str + String(key) + '=' + obj[key] + '&';
+            }
+    });
+    return(str.substring(0, str.length - 1));
+};
 
 window.searchString = searchString;
 
 export default searchString;
+
