55e48b2a8c7bb1fdec3818df003f4ea57d47fa0d
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-08 16:13:32 +0300

    add program arrays-sort

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,7 +1,23 @@
 function arraysSort(arr) {
-  // Напиши свой код здесь
+    arr.sort((a, b) => {
+        if (arrayMax(a) > arrayMax(b)) {
+            return 1;
+        } else if(arrayMax(a) < arrayMax(b)) {
+            return -1;
+        } else {
+            return 0;
+        }
+    } );
+    return arr;
 };
-
+const arrayMax = (arr) => {
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    } return max;
+}
 window.arraysSort = arraysSort;
 
 export default arraysSort;
