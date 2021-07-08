6c7beef375476623bd5ce87e5adb2851c6be7c5c
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-08 13:25:58 +0300

    add program full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,17 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+    let result = 0;
+    try {
+        for (let j = 0; j<arguments.length; j++)
+            if (typeof arguments[j] !== 'number') {
+                throw new Error('Wrong arguments type!');
+            }
+        for (let i = 0; i < arguments.length; i++) {
+            result += arguments[i];
+        }
+        return result;
+    } catch (error) {
+        throw new Error(error);
+    }
 }
 
 window.fullSum = fullSum;
