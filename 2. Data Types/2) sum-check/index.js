5ac211f11c77565174383469a14ad16ca870f0b4
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-08 09:53:42 +0300

    sum-check v2

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -3,11 +3,9 @@ function sumCheck(a, b) {
       if (typeof a === 'number' && typeof b === 'number') {
           return a + b;
       }
-      throw new Error("Wrong arguments type!");
-
-  }
-  catch (error) {
-      throw new Error(error)
+      throw new Error('Wrong arguments type!');
+  } catch (error) {
+      throw new Error(error);
   }
 }
 
