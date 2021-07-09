373bcd036e0723495cbcdb8f33565dd38daae1e2
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-09 15:01:59 +0300

    add program arrays-sort

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,8 +1,10 @@
 function findPair(arr) {
     for (let i = 0; i < arr.length; i++) {
-        for(let j = arr.length ; j >= 0; j--) {
-            if (arr[i] === arr[j] && j !== i ) {
-                return arr[i];
+        for(let j = arr.length; j > i; j--) {
+            if (arr[i] === arr[j]) {
+                console.log(i,j);
+                return arr[j];
+
             }
         }
     }
@@ -12,3 +14,16 @@ function findPair(arr) {
 window.findPair = findPair;
 
 export default findPair;
+
+/*function findPair(arr) {
+    let findDuble = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+    let arr2 = findDuble(arr)
+    if (arr2.length !== 0) {
+        return arr2[0];
+    }
+  return null;
+};
+
+window.findPair = findPair;
+
+export default findPair;*/
