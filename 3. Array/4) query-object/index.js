4d7ee591cabf8ac068d06177fb7f524562419a72
Ruslan <chopozov.exceedteam@gmail.com>
2021-07-09 10:53:02 +0300

    add program query-object

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,5 +1,30 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
+    let a = query.slice(1);
+    let b = a.split('&').join(',').split('=').join(',').split(',');
+    let keys = [];
+    let values = [];
+    for (let i = 0; i < b.length; i++) {
+        if (i % 2 === 0 || i === 0) {
+            keys.push(b[i]);
+        } else {
+            values.push((b[i]));
+        }
+    }
+    for (let j = 0; j < values.length; j++) {
+        if (values[j] === 'true') {
+            values[j] = true;
+        } else if (values[j] === 'false') {
+            values[j] = false;
+        } else if (values[j] / 1 === Number(values[j])) {
+            values[j] = Number(values[j]);
+        }
+
+    }
+        let result = {};
+        for (let i = 0; i < keys.length; i++) {
+            result[keys[i]] = values[i];
+        }
+        return result;
 };
 
 window.queryToObject = queryToObject;
